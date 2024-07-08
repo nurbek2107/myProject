cb539bfa54018bf8d12dd671444235674da2c30b
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-07-08 22:47:08 +0500

    buttons solved

--- src/components/WhiteBtn.jsx
+++ src/components/WhiteBtn.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function WhiteBtn({ text }) {
     return (
-        <button className='btn bg-white text-[#359740] px-[33px]'>
+        <button className='btn bg-white text-[#359740] hover:bg-[#cfcdcd] px-[33px]'>
             {text}
         </button>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,23 +6,15 @@ function Home() {
   return (
     <section className="main-container mt-8 mb-4">
       <div className="hero-section flex justify-between bg-color-lightgreen py-[50px] pl-[76px] relative">
-        <div className="w-[650px]">
-          <h1 className="text-5xl leading-[54px] -tracking-[1px] font-semibold mb-3 text-primary">
         <div className="w-[650px] text-primary">
           <h1 className="text-5xl leading-[54px] -tracking-[1px] font-semibold mb-3">
             SEEDRA Basil Seeds for Indoor and Outdoor Planting
           </h1>
-          <p className="text-base leading-[28px] font-light text-[#1F2533] mb-[21px]  ">
-            Be sure of our quality - the freshest batches of this season.
-            Non-GMO, Heirloom - our seeds were tested and have the best
-            germination ratings. Your easy growing experience is our guarantee
+          <p className="text-base leading-[28px] font-light text-[#1F2533] mb-[21px] ">
+            Be sure of our quality - the freshest batches of this season. Non-GMO, Heirloom - our seeds were tested and have the best germination ratings. Your easy growing experience is our guarantee
           </p>
           <div className="flex items-center mb-[25px]">
-            <img
-              className="mr-[6px]"
-              src="../assets/svg/ant-design_fire-filled.svg"
-              alt="fire"
-            />
+            <img className="mr-[6px]" src="../assets/svg/ant-design_fire-filled.svg" alt="fire" />
             <p className="mr-[21px] font-semibold text-[42px] text=[#1F2533] ">
               $12.56
             </p>
@@ -31,8 +23,8 @@ function Home() {
             </p>
           </div>
           <div className="flex gap-3">
-            <GreenBtn text="Add to card" />
-            <WhiteBtn text="Discover" />
+            <GreenBtn text='Add to card' />
+            <WhiteBtn text='Discover' />
           </div>
         </div>
 
@@ -40,11 +32,7 @@ function Home() {
           <img className="" src="../assets/png/seed-packet.png" alt="packet" />
         </div>
 
-        <img
-          className="absolute bottom-0 z-[1] right-0"
-          src="../assets/png/hero-leaves.png"
-          alt="leaves"
-        />
+        <img className="absolute bottom-0 z-[1] right-0" src="../assets/png/hero-leaves.png" alt="leaves" />
       </div>
     </section>
   );
