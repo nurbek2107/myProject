d45be6e87016eef26ed77c0692b105d75e307013
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-07-09 02:52:21 +0500

    another section added

---
+++ src/components/AllProducts.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function AllProducts() {
+    return (
+        <>
+            <section>
+                
+            </section>
+        </>
+    )
+}
+
+export default AllProducts
