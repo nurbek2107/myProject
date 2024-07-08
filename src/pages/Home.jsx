aad82f5030200c0536cd112195e22ec4c62ba23f
Fayyozbek <usmonovbahriddin108@gmail.com>
2024-07-08 19:32:42 +0500

    [feat] home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,29 @@
+//hooks
 function Home() {
   return (
-    <div>
-      <h1>Welcome to Home</h1>
+    <div className="main-container">
+      <div className="hero-section flex justify-between">
+        <div className="w-[650px] h-[355px]">
+          <h1>SEEDRA Basil Seeds for Indoor and Outdoor Planting</h1>
+          <p>
+            Be sure of our quality - the freshest batches of this season.
+            Non-GMO, Heirloom - our seeds were tested and have the best
+            germination ratings. Your easy growing experience is our guarantee
+          </p>
+          <div>
+            <img src="../assets/svg/ant-design_fire-filled.svg" alt="" />
+            <p>$12.56</p>
+            <span>$15.56</span>
+          </div>
+          <div>
+            <button>Add to card</button>
+            <button>Discover</button>
+          </div>
+        </div>
+        <div>hello</div>
+      </div>
     </div>
   );
 }
 
-export default Home
+export default Home;
