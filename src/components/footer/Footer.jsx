e07dc85509bfb677a2e108b5222b7fcb63f16d48
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-07-08 18:27:13 +0500

    footers link

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,46 @@
+import { Link } from "react-router-dom"
+
 function Footer() {
   return (
     <footer>
-        <div className="main-container">
-          
+      <div className="main-container">
+        <div>
+          <ul>
+            <li>
+              <Link to={`/`}>
+                all products
+              </Link>
+            </li>
+            <li>
+              <Link to={`/`}>
+                about seedra
+              </Link>
+            </li>
+            <li>
+              <Link to={`/`}>
+                our block
+              </Link>
+            </li>
+            <li>
+              <Link to={`/`}>
+                <img src="../assets" alt="logo" />
+              </Link>
+            </li>
+            <li>
+              <Link to={`/`}>
+                Terms&Conditions
+              </Link>
+            </li>
+            <li>
+              <Link to={`/`}>
+                Privacy Policy
+              </Link>
+            </li>
+          </ul>
         </div>
-    </footer> 
+        <div></div>
+      </div>
+    </footer>
   )
 }
 
